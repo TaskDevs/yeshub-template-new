b965db2c46349aa4f5316ebbcf95506712d3e4a6
halima-yusif-attah <ylheema19@gmail.com>
2025-01-20 15:37:44 +0000

    fix:fixed scroll to top behavior

--- src/app/common/floating/all-demo.jsx
+++ src/app/common/floating/all-demo.jsx
@@ -1,13 +1,13 @@
-function AllDemoFloatingMenu(props) {
-    return (
-        <>
-            <a href="#"
-                id="all-demo-open"
-                className="all-demos-view"
-                onClick={props.onClick}
-            >All Demo</a>
-        </>
-    )
-}
+// function AllDemoFloatingMenu(props) {
+//     return (
+//         <>
+//             <a href="#"
+//                 id="all-demo-open"
+//                 className="all-demos-view"
+//                 onClick={props.onClick}
+//             >All Demo</a>
+//         </>
+//     )
+// }
 
-export default AllDemoFloatingMenu;
+// export default AllDemoFloatingMenu;

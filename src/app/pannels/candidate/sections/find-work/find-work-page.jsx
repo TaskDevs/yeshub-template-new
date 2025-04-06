4cbe01c7e5f13559bd9f9d7879661264219c894f
halima-yusif-attah <ylheema19@gmail.com>
2025-04-06 23:31:07 +0000

    fix: updated find-work section

--- src/app/pannels/candidate/sections/find-work/find-work-page.jsx
+++ src/app/pannels/candidate/sections/find-work/find-work-page.jsx
@@ -10,7 +10,7 @@ import CanJobCard from "../../components/can-job-card";
 import ProfileInfoSection from "./profile-info-section";
 import { ProfileApiData } from "../../../../context/user-profile/profileContextApi";
 import { JobApiData } from "../../../../context/jobs/jobsContextApi";
-import Loader from "../../../../common/loader";
+
 
 
 function FindWorkPage() {
@@ -30,9 +30,7 @@ function FindWorkPage() {
 
   
 
-  if (!profileData?.id) {
-    return (<div><Loader/></div>)
-  }
+ 
 
   return (
     <div className="tw-css  min-h-screen">
